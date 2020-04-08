adef96ea96f4e712b57a04238924d6295ff1794a
Ryan Palmer <ryanpalmermultimedia@gmail.com>
2020-04-08 15:07:47 +0100

    Added instrunctions to debouncer

--- FunctionalBlazor/FunctionalXamarin/Common/Debounce.cs
+++ FunctionalBlazor/FunctionalXamarin/Common/Debounce.cs
@@ -5,6 +5,32 @@ using System.Threading.Tasks;
 
 namespace FunctionalXamarin.Common
 {
+    /// <summary>
+    /// 
+    ///  Use this to stop text input spamming IProgram with messages which also
+    ///  can cause loops if you use two way bindings (when two changes are sent 
+    ///  before  any come back they start fighting to overwrite the property).
+    ///  
+    ///  Using this, each action waits for a moment before executing,
+    ///  and any further actions cancel and replace it, so you only actually
+    ///  run the final action.
+    /// 
+    ///  For a string property in a ViewModel, you can do:
+    /// 
+    ///  Action<string>? _debouncedTitle;
+    ///  void DoUpdateTitle(string title)
+    ///  {
+    ///      _debounceTitle ??= new Action<string>(s =>
+    ///      {
+    ///          _program.Post(
+    ///              ProgramMsg.NewSubPageMessage(
+    ///                  NewSubPageMsg.NewTitleUpdated(s)));
+    ///      }).Debounce();
+    ///          
+    ///      _debouncedTitle?.Invoke(title);
+    ///  }
+    ///  
+    /// </summary>
     public static class Debouncer
     {
         public static Action<T> Debounce<T>(this Action<T> func, int milliseconds = 300)
